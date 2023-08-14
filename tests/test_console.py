0ea06af9c3e923d03e0b8d445743b73b0993aadd
abdel-moaty <ahmed.h.abdelmoaty@gmail.com>
2023-08-14 15:48:48 -0700

    Resolve failed unit tests in test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -37,64 +37,92 @@ class TestHBNBCommand_help(unittest.TestCase):
     """Unittests for testing help messages of the HBNB command interpreter."""
 
     def test_help_quit(self):
-        h = "Quit command to exit the program."
+        h = ("Exit the command-line interface.\n\n"
+             "        Args:\n"
+             "            arg (str): The command argument.\n\n"
+             "        Returns:\n"
+             "            bool: True to exit the interface.")
         with patch("sys.stdout", new=StringIO()) as output:
             self.assertFalse(HBNBCommand().onecmd("help quit"))
             self.assertEqual(h, output.getvalue().strip())
 
     def test_help_create(self):
-        h = ("Usage: create <class>\n        "
-             "Create a new class instance and print its id.")
+        h = ("Create a new instance of a specified class.\n\n"
+             "        Args:\n"
+             "            arg (str): The class name.\n\n"
+             "        Returns:\n"
+             "            None")
         with patch("sys.stdout", new=StringIO()) as output:
             self.assertFalse(HBNBCommand().onecmd("help create"))
             self.assertEqual(h, output.getvalue().strip())
 
     def test_help_EOF(self):
-        h = "EOF signal to exit the program."
+        expected_output = ("Handle the End-of-File signal.\n\n"
+                           "        Args:\n"
+                           "            arg (str): The command argument.\n\n"
+                           "        Returns:\n"
+                           "            bool: True to exit the interface.")
         with patch("sys.stdout", new=StringIO()) as output:
             self.assertFalse(HBNBCommand().onecmd("help EOF"))
-            self.assertEqual(h, output.getvalue().strip())
+            actual_output = output.getvalue().strip()
+            self.assertEqual(expected_output, actual_output)
 
     def test_help_show(self):
-        h = ("Usage: show <class> <id> or <class>.show(<id>)\n        "
-             "Display the string representation of a class instance of"
-             " a given id.")
+        e = ("Display information about a specific instance.\n\n"
+             "        Args:\n"
+             "            arg (str): The class name and instance ID.\n\n"
+             "        Returns:\n"
+             "            None")
         with patch("sys.stdout", new=StringIO()) as output:
             self.assertFalse(HBNBCommand().onecmd("help show"))
-            self.assertEqual(h, output.getvalue().strip())
+            a = output.getvalue().strip()
+            self.assertEqual(e, a)
 
     def test_help_destroy(self):
-        h = ("Usage: destroy <class> <id> or <class>.destroy(<id>)\n        "
-             "Delete a class instance of a given id.")
+        e = ("Delete a specific instance.\n\n"
+             "        Args:\n"
+             "            arg (str): The class name and instance ID.\n\n"
+             "        Returns:\n"
+             "            None")
         with patch("sys.stdout", new=StringIO()) as output:
             self.assertFalse(HBNBCommand().onecmd("help destroy"))
-            self.assertEqual(h, output.getvalue().strip())
+            a = output.getvalue().strip()
+            self.assertEqual(e, a)
 
     def test_help_all(self):
-        h = ("Usage: all or all <class> or <class>.all()\n        "
-             "Display string representations of all instances of a given class"
-             ".\n        If no class is specified, displays all instantiated "
-             "objects.")
+        expected = ("Display information about all instances or instances of\n"
+                    "        a specific class.\n\n"
+                    "        Args:\n"
+                    "            arg (str): The optional class name.\n\n"
+                    "        Returns:\n"
+                    "            None")
         with patch("sys.stdout", new=StringIO()) as output:
             self.assertFalse(HBNBCommand().onecmd("help all"))
-            self.assertEqual(h, output.getvalue().strip())
+            actual = output.getvalue().strip()
+            self.assertEqual(expected, actual)
 
     def test_help_count(self):
-        h = ("Usage: count <class> or <class>.count()\n        "
-             "Retrieve the number of instances of a given class.")
+        expected = ("Count the number of instances of a specific class.\n\n"
+                    "        Args:\n"
+                    "            arg (str): The class name.\n\n"
+                    "        Returns:\n"
+                    "            None")
         with patch("sys.stdout", new=StringIO()) as output:
             self.assertFalse(HBNBCommand().onecmd("help count"))
-            self.assertEqual(h, output.getvalue().strip())
+            actual = output.getvalue().strip()
+            self.assertEqual(expected, actual)
 
     def test_help_update(self):
-        h = ("Usage: update <class> <id> <attribute_name> <attribute_value> or"
-             "\n       <class>.update(<id>, <attribute_name>, <attribute_value"
-             ">) or\n       <class>.update(<id>, <dictionary>)\n        "
-             "Update a class instance of a given id by adding or updating\n   "
-             "     a given attribute key/value pair or dictionary.")
+        e = ("Update attributes of a specific instance.\n\n"
+             "        Args:\n"
+             "            arg (str): The class name, instance ID, "
+             "attribute name, and value.\n\n"
+             "        Returns:\n"
+             "            None")
         with patch("sys.stdout", new=StringIO()) as output:
             self.assertFalse(HBNBCommand().onecmd("help update"))
-            self.assertEqual(h, output.getvalue().strip())
+            a = output.getvalue().strip()
+            self.assertEqual(e, a)
 
     def test_help(self):
         h = ("Documented commands (type help <topic>):\n"
